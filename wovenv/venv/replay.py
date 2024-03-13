943db733bf6ac478de28e1309e99415656a48a1c
Semen Kukharenko <localhost@mail.ru>
2024-03-13 14:53:15 +0300

    Added usage of PriorityReplayBuffer. Setup (env) file was made

--- wovenv/venv/replay.py
+++ wovenv/venv/replay.py
@@ -1,38 +1,23 @@
-import random
+import torch
 from wovenv.venv.snapshot import SnapShot, Action
+from torchrl.data.replay_buffers import PrioritizedReplayBuffer, ListStorage
 
 class Replay:
     def __init__(self, size: int) -> None:
-        self.buffer, self.priority = [], []
-        self.size = size
+        self.data = dict()
+        self.buffer = PrioritizedReplayBuffer(alpha=0.7, beta=0.9, storage=ListStorage(size))
 
     def len(self) -> int:
         return len(self.buffer)
-
-    def pop(self):
-        index = min(range(self.len()), key=lambda x: self.priority[x])
-        self.buffer.pop(index)
-        self.priority.pop(index)
-
-    def add(self, s: SnapShot, a: Action, ns: SnapShot, r: int, d: bool, p: float) -> None:
-        self.buffer.append((s, a, ns, r, d))
-        self.priority.append(p)
-        if self.len() > self.size:
-            self.pop()
-
-    def sample(self, batch_size: int):
-        values = [random.uniform(0, 1) for _ in range(batch_size)]
-        sls, idx = [], []
-        values.sort()
-        ind, tmp, sm = 0, 0, sum(self.priority)
-        for i in range(self.len()):
-            tmp += self.priority[i] / sm
-            while ind < batch_size and values[ind] <= tmp + 0.0001:
-                idx.append(i)
-                sls.append(self.buffer[i])
-                ind += 1
-        return sls, idx
     
-    def update_priority(self, prs, idx):
-        for i, p in zip(idx, prs):
-            self.priority[i] = p
+    def add(self, s: SnapShot, a: Action, ns: SnapShot, r: int, d: bool, p: float) -> None:
+        index = self.buffer.add(1)
+        self.data[index] = (s, a, ns, r, d)
+        self.buffer.update_priority(index, p)
+
+    def sample(self, sample_size: int) -> tuple[list[tuple], torch.Tensor]:
+        result, info = self.buffer.sample(sample_size, return_info=True)
+        return ([self.data[r.item()] for r in info['index']], info['index'])
+    
+    def update_priority(self, prs, idx) -> None:
+        self.buffer.update_priority(idx, prs)
